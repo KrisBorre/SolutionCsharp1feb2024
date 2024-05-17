fd53a4522cc5603953fe0db300cb6e1fcbc86987
kris_borre <kris_borre@DESKTOP-V0LQE95>
2024-05-17 13:42:29 +0200

    Newtonsoft

---
+++ ConsoleHttpClientDisney18may2024/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsoleHttpClientDisney18may2024
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // https://disneyapi.dev/docs/
+
+            using var client = new HttpClient();
+            string name = "Bambi";
+
+            string requestUri = "https://api.disneyapi.dev/character?name=" + name;
+
+            HttpResponseMessage response = await client.GetAsync(requestUri);
+
+            response.EnsureSuccessStatusCode();
+
+            var myJsonResponse = await response.Content.ReadAsStringAsync();
+
+            //await Console.Out.WriteLineAsync(myJsonResponse);
+
+            // https://json2csharp.com/
+
+            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+            List<Datum> data = myDeserializedClass.data;
+
+            foreach (Datum item in data)
+            {
+                List<string> films = item.films;
+                List<object> allies = item.allies;
+                List<object> enemies = item.enemies;
+
+                foreach (string film in films)
+                {
+                    await Console.Out.WriteLineAsync(film);
+                }
+            }
+
+            /*
+            Bambi (film)
+            Bambi II
+            Perri
+            The Rescuers
+            Who Framed Roger Rabbit
+            Mickey's Magical Christmas: Snowed in at the House of Mouse
+            A Goofy Movie
+            The Reluctant Dragon
+            Zootopia
+            Ralph Breaks the Internet
+            Bambi (live-action film)
+            Bambi (film)
+            The Sword in the Stone
+            The Jungle Book
+            The Rescuers
+            Who Framed Roger Rabbit
+            Beauty and the Beast (1991 film)
+            Bambi II
+            */
+
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleHttpClientDisney18may2024/Root.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleHttpClientDisney18may2024
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Datum
+    {
+        public int _id { get; set; }
+        public List<string> films { get; set; }
+        public List<string> shortFilms { get; set; }
+        public List<string> tvShows { get; set; }
+        public List<string> videoGames { get; set; }
+        public List<string> parkAttractions { get; set; }
+        public List<object> allies { get; set; }
+        public List<object> enemies { get; set; }
+        public string sourceUrl { get; set; }
+        public string name { get; set; }
+        public string imageUrl { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public string url { get; set; }
+        public int __v { get; set; }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int totalPages { get; set; }
+        public object previousPage { get; set; }
+        public object nextPage { get; set; }
+    }
+
+    public class Root
+    {
+        public Info info { get; set; }
+        public List<Datum> data { get; set; }
+    }
+
+}
