f7eeb8063e478b7707a13e31779e65044e36f98e
kris_borre <kris_borre@DESKTOP-V0LQE95>
2024-08-02 20:38:35 +0200

    adiabatic invariant

--- ConsoleArxiv24may2024/Program.cs
+++ ConsoleArxiv24may2024/Program.cs
@@ -14,21 +14,157 @@ namespace ConsoleArxiv24may2024
 
             string word1 = "adiabatic";
             string word2 = "invariant";
+            string word3 = "elliptic";
 
-            // 963 abstracts
             List<Article> allArticles = dbContext.Articles
                 .Include(a => a.Contributions)
                 .Include(a => a.Links)
                 .ToList();
 
-            Console.WriteLine("Number of Articles: " + allArticles.Count); // 1531
+            Console.WriteLine("Number of Articles: " + allArticles.Count); // 2445
 
             List<Contribution> allContributions = dbContext.Contributions.ToList();
             List<Link> allLinks = dbContext.Links.ToList();
 
-            //var selectedArticles1 = allArticles.FindAll(x => x.Title.Contains(word1)); // 32
+            var selectedArticles1 = allArticles.FindAll(x => x.Title.Contains(word1, StringComparison.OrdinalIgnoreCase));  // 126
 
-            //var selectedArticles2 = allArticles.FindAll(x => x.Title.Contains(word2)); // 23
+            var selectedArticles2 = allArticles.FindAll(x => x.Title.Contains(word2, StringComparison.OrdinalIgnoreCase));  // 67
+
+            var bothArticles1 = allArticles.FindAll(x => (x.Title.Contains(word1, StringComparison.OrdinalIgnoreCase) && x.Title.Contains(word2, StringComparison.OrdinalIgnoreCase))); // 20
+
+            foreach (var article in bothArticles1)
+            {
+                string[] titles = article.Title.Split("\n");
+                foreach (var t in titles)
+                {
+                    Console.WriteLine(t);
+                }
+                Console.WriteLine();
+                //Console.WriteLine(article.Abstract + "\n");
+                foreach (var link in article.Links)
+                {
+                    if (link.Hyperlink.Contains("pdf"))
+                    {
+                        Console.WriteLine(link.Hyperlink);
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+
+            /*
+            Invariant Perturbation Theory of Adiabatic Process
+
+            http://arxiv.org/pdf/0706.0299v3
+
+
+            Adiabatic invariants for the dynamics of rotating 3-dimensional fluid
+
+            http://arxiv.org/pdf/1206.1018v1
+
+
+            General formulas for adiabatic invariants in nearly-periodic Hamiltonian
+              systems
+
+            http://arxiv.org/pdf/2005.00634v1
+
+
+            On adiabatic invariant in generalized Galileon theories
+
+            http://arxiv.org/pdf/1505.04670v1
+
+
+            On the interplay between Noether's theorem and the theory of adiabatic
+              invariants
+
+            http://arxiv.org/pdf/2012.08853v1
+
+
+            Adiabatic invariants drive rhythmic human motion in variable gravity
+
+            http://arxiv.org/pdf/1906.08686v1
+
+
+            The Rayleigh-Lorentz Invariant and Optimal Adiabatic Qubit-Information
+              Detection for Superconducting Qubit Resonators
+
+            http://arxiv.org/pdf/2007.10671v1
+
+
+            Robust stimulated Raman shortcut-to-adiabatic passage by invariant-based
+              optimal control
+
+            http://arxiv.org/pdf/2103.01386v1
+
+
+            On the accuracy of conservation of adiabatic invariants in slow-fast
+              systems
+
+            http://arxiv.org/pdf/1103.1595v1
+
+
+            Change in the adiabatic invariant in a nonlinear Landau-Zener problem
+
+            http://arxiv.org/pdf/0910.3061v2
+
+
+            Borel summation of adiabatic invariants
+
+            http://arxiv.org/pdf/math/0608315v1
+
+
+            Adiabatic invariants of the extended KdV equation
+
+            http://arxiv.org/pdf/1512.01194v3
+
+
+            Dynamical invariant formalism of shortcuts to adiabaticity
+
+            http://arxiv.org/pdf/2209.04367v1
+
+
+            Exact Analysis of the Adiabatic Invariants in Time-Dependent Harmonic
+              Oscillator
+
+            http://arxiv.org/pdf/nlin/0506033v1
+
+
+            The invariant based investigation of Shortcut to Adiabaticity for
+              Quantum Harmonic Oscillators under time varying frictional force
+
+            http://arxiv.org/pdf/1910.04575v2
+
+
+            Higher-derivative harmonic oscillators: stability of classical dynamics
+              and adiabatic invariants
+
+            http://arxiv.org/pdf/1811.07733v2
+
+
+            On the number of independent adiabatic invariants for gyrating particles
+
+            http://arxiv.org/pdf/0707.2471v1
+
+
+            Adiabatic limit of the eta invariant over cofinite quotient of PSL(2,R)
+
+            http://arxiv.org/pdf/0705.4506v1
+
+
+            Adiabatic invariants for the regular region of the Dicke model
+
+            http://arxiv.org/pdf/1611.07863v1
+
+
+            Change in the adiabatic invariant in a nonlinear two-mode model of
+              Feshbach resonance passage
+
+            http://arxiv.org/pdf/nlin/0611007v2
+            */
+
+            var bothArticles2 = allArticles.FindAll(x => ((x.Title.Contains(word1, StringComparison.OrdinalIgnoreCase) || x.Abstract.Contains(word1, StringComparison.OrdinalIgnoreCase)) && (x.Title.Contains(word2, StringComparison.OrdinalIgnoreCase) || x.Abstract.Contains(word2, StringComparison.OrdinalIgnoreCase)))); // 97
+
+            var threeArticles1 = bothArticles2.FindAll(x => x.Abstract.Contains(word3, StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("The number of articles about adiabatic invariants and elliptic ... : " + threeArticles1.Count); // 0
 
             var grouped = allContributions.GroupBy(c => c.Author);
 
@@ -37,24 +173,16 @@ namespace ConsoleArxiv24may2024
                 if (group.Count() > 6) Console.WriteLine($"{group.Key} ({group.Count()})");
             }
             /*
-A. I. Neishtadt (8)
-Jérôme Darmont (16)
-Fadila Bentayeb (7)
-T. Mart (10)
-James Atwood (7)
+            G. Sardanashvily (8)
+            Alain J. Brizard (11)
+            A. I. Neishtadt (8)
+            Jérôme Darmont (16)
+            Fadila Bentayeb (7)
+            T. Mart (10)
+            James Atwood (7)
             */
 
-            /*
-            J. W. Burby (4)
-            G. Sardanashvily (4)
-            Alain J. Brizard (5)
-            A. P. Itin (4)
-            A. I. Neishtadt (8)
-            A. A. Vasiliev (5)
-            Danielle S. Bassett (4)
-            Nick Patterson (4)
-            David Reich (4)
-            */
+            #region announcement date and submission date
 
             int earlier = allArticles.FindAll(a => a.DateTime1 < a.DateTime2).Count;
             Console.WriteLine("earlier = " + earlier);
@@ -68,17 +196,22 @@ James Atwood (7)
             same = 566
             later = 199
             */
+            #endregion
 
+            #region github
             List<Article> githubArticles = allArticles.FindAll(a => a.Abstract.Contains("github"));
 
             foreach (var article in githubArticles)
             {
                 string[] titles = article.Title.Split("\n");
-                Console.WriteLine(titles[0]);
-                Console.WriteLine(article.Abstract + "\n");
+                //Console.WriteLine(titles[0]);
+                //Console.WriteLine(article.Abstract + "\n");
             }
 
-            /*           
+            /*         
+            WW-Nets: Dual Neural Networks for Object Detection
+            https://github.com/mkebrahimpour
+            
             DGoT: Dynamic Graph of Thoughts for Scientific Abstract Generation
             https://github.com/JayceNing/DGoT
             Python
@@ -87,6 +220,12 @@ James Atwood (7)
             https://github.com/TaatiTeam/OCW
             Python
             
+            Joint Learning of Salient Object Detection, Depth Estimation and Contour
+            https://github.com/Xiaoqi-Zhao-DLUT/MMFT
+
+            VISLA Benchmark: Evaluating Embedding Sensitivity to Semantic and
+            https://github.com/Sri-Harsha/visla_benchmark
+
             MORE: Simultaneous Multi-View 3D Object Recognition and Pose Estimation
             https://github.com/SubhadityaMukherjee/more_mvcnn
             Python
@@ -106,7 +245,103 @@ James Atwood (7)
             Identifying recombination hotspots using population genetic data
             http://github.com/auton1/LDhot
             C++
+
+            MATES: Model-Aware Data Selection for Efficient Pretraining with Data
+            https://github.com/cxcscmu/MATES
+
+            Fin-Fact: A Benchmark Dataset for Multimodal Financial Fact Checking and
+            https://github.com/IIT-DM/Fin-Fact/
+
+            Label-Efficient Learning in Agriculture: A Comprehensive Review
+            https://github.com/DongChen06/Label-efficient-in-Agriculture
+
+            TorchGeo: Deep Learning With Geospatial Data
+            https://github.com/microsoft/torchgeo
+
+            FlightScope: A Deep Comprehensive Assessment of Aircraft Detection
+            https://github.com/toelt-llc/FlightScope_Bench
+
+            Public Computer Vision Datasets for Precision Livestock Farming: A
+            https://github.com/Anil-Bhujel/Public-Computer-Vision-Dataset-A-Systematic-Survey
+
+            Landmark Detection and 3D Face Reconstruction for Caricature using a
+            https://github.com/Juyong/CaricatureFace
+
+            Weakly-supervised Caricature Face Parsing through Domain Adaptation
+            https://github.com/ZJULearning/CariFaceParsing
+
+            Self-supervised transformer-based pre-training method with General Plant
+            https://github.com/WASSER2545/GPID-22
+
+            Component-aware anomaly detection framework for adjustable and logical
+            https://github.com/liutongkun/ComAD
+
+            Cross-Domain Graph Anomaly Detection via Anomaly-aware Contrastive
+            https://github.com/QZ-WANG/ACT
+
+            RealNet: A Feature Selection Network with Realistic Synthetic Anomaly
+            https://github.com/cnulab/RealNet
+
+            Towards Scalable 3D Anomaly Detection and Localization: A Benchmark via
+            https://github.com/Chopper-233/Anomaly-ShapeNet
+
+            Exploiting Structural Consistency of Chest Anatomy for Unsupervised
+            https://github.com/MrGiovanni/SimSID
+
+            Exploring CLIP for Assessing the Look and Feel of Images
+            https://github.com/IceClear/CLIP-IQA
+
+            Universal Instance Perception as Object Discovery and Retrieval
+            https://github.com/MasterBin-IIAU/UNINEXT
+
+            PreprintMatch: a tool for preprint publication detection applied to
+            https://github.com/PeterEckmann1/preprint-match
+
+            Image-to-Image Translation via Group-wise Deep Whitening-and-Coloring
+            https://github.com/WonwoongCho/GDWCT
+
+            Online Analytic Exemplar-Free Continual Learning with Large Models for
+            https://github.com/ZHUANGHP/Analytic-continual-learning
+
+            ScrewNet: Category-Independent Articulation Model Estimation From Depth
+            https://pearl-utexas.github.io/ScrewNet/
+
+            Distributional Depth-Based Estimation of Object Articulation Models
+            https://pearl-utexas.github.io/DUST-net/
+
+            WordStylist: Styled Verbatim Handwritten Text Generation with Latent
+            https://github.com/koninik/WordStylist
+
+            GR-RNN: Global-Context Residual Recurrent Neural Networks for Writer
+            https://github.com/shengfly/writer-identification
+
+            EmoGen: Eliminating Subjective Bias in Emotional Music Generation
+            https://ai-muzic.github.io/emogen/
+            https://github.com/microsoft/muzic/
+
+            Using Emotion Embeddings to Transfer Knowledge Between Emotions
+            https://github.com/gchochla/Demux-MEmo
+
+            EmoVIT: Revolutionizing Emotion Insights with Visual Instruction Tuning
+            https://github.com/aimmemotion/EmoVIT}
+
+            DRT: A Lightweight Single Image Deraining Recursive Transformer
+            https://github.com/YC-Liang/DRT
+
+            Real-time Transformer-based Open-Vocabulary Detection with Efficient
+            https://github.com/om-ai-lab/OmDet
+
+            Data-Efficient Unsupervised Interpolation Without Any Intermediate Frame
+            https://github.com/jungeun122333/UVI-Net
+
+            Decoding the End-to-end Writing Trajectory in Scholarly Manuscripts
+            https://minnesotanlp.github.io/REWARD_demo/
+
+            Learning to Localize Actions from Moments
+            https://github.com/FuchenUSTC/AherNet
+
             */
+            #endregion
 
             Console.WriteLine();
 
@@ -249,8 +484,8 @@ James Atwood (7)
                 }
             }
 
-            Console.WriteLine("Number of articles with questions in the Title: " + questionsInTitle.Count); // 28
-            Console.WriteLine("Number of articles with questions in the Abstract: " + questionsInAbstract.Count); // 31
+            Console.WriteLine("Number of articles with questions in the Title: " + questionsInTitle.Count); // 37
+            Console.WriteLine("Number of articles with questions in the Abstract: " + questionsInAbstract.Count); // 40
             Console.WriteLine("Number of articles with questions in the Abstract and the Title: " + questionsInAbstractAndTitle.Count); // 2
             Console.WriteLine();
 
@@ -262,11 +497,11 @@ James Atwood (7)
 
             Console.WriteLine();
 
-            foreach (Article article in questionsInAbstract)
-            {
-                Console.WriteLine(article.Abstract);
-                Console.WriteLine();
-            }
+            //foreach (Article article in questionsInAbstract)
+            //{
+            //    Console.WriteLine(article.Abstract);
+            //    Console.WriteLine();
+            //}
 
             Console.Read();
         }
